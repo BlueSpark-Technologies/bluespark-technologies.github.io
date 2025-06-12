cfda37da0470f4f0e8e852483b55db1b70c5723c
Horatiu <horatiu.maiereanu@bluespark.ro>
2025-06-12 11:54:44 +0300

    disabled compression and added asset prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
+  compress: false,
+  /**
+   * Set assetPrefix to our production domain so that assets are served from the correct path.
+   * This is needed for deployments to a CDN.
+   * In development, this is an empty string.
+   */
+  assetPrefix: isProd ? process.env.NEXT_PUBLIC_ASSET_PREFIX : '',
 }
 
 module.exports = nextConfig
